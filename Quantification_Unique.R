d159c169d5c35be4b895a7e058b059b1bfb0e723
Mollie Peters <mjpete11@asu.edu>
2019-05-17 11:38:31 -0700

    config for sample replicates only

--- Quantification_Unique.R
+++ Quantification_Unique.R
@@ -1,17 +1,29 @@
-# Create config of samples that worked with Salmon, without sample replicates.
+# Create config of samples that worked with Salmon, without sample replicates and only sample replicates.
 
 library(jsonlite)
 library(stringr)
 
-x <- read_json("/scratch/mjpete11/GTEx/Dup_Quantification.config.json")
+Quant_Files <- read_json("/scratch/mjpete11/GTEx/Dup_Quantification.config.json")
 
 # Check if samples have same individual ID
 # If same ID, keep only one.
-tmp <- lapply(x,function(y){
+No_Reps <- lapply(Quant_Files, function(y){
     c <- y[-which(duplicated(str_extract(y,"GTEX-[0-9A-Z]+")))]
-    if(length(c)==0){y}else{c}
+    if (length(c)==0) {y} else {c}
 })
 
-write_json(tmp,"/scratch/mjpete11/GTEx/No_Dup_Quantification.config.json")
+# Write config
+write_json(No_Reps,"/scratch/mjpete11/GTEx/Quantification.config.json")
+
+# Identify sample replicates
+Filt_Quant <- Quant_Files[-c(1:3)];
+
+Reps <- lapply(Filt_Quant, function(y){
+  c <- y[which(duplicated(str_extract(y,"GTEX-[0-9A-Z]+")))]
+  if (length(c)==0) {NA} else {c}
+})
+
+# Write config
+write_json(Reps,"/scratch/mjpete11/GTEx/Rep_Only_Quantification.config.json")
 
 
