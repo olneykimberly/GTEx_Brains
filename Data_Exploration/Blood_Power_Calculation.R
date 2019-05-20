26210292e8dd5c6811c92d78a3983881ab68b376
Mollie Peters <mjpete11@asu.edu>
2019-05-20 11:33:49 -0700

    Kept total blood samples in output

--- Data_Exploration/Blood_Power_Calculation.R
+++ Data_Exploration/Blood_Power_Calculation.R
@@ -4,19 +4,16 @@
 METADATA <- file.path("/scratch/mjpete11/GTEx/", "Metadata.csv")
 GTEX_SRA_CONFIG <- "/scratch/mjpete11/GTEx/Spinal_Cord/Male_Spinal_Cord.config.json"
 WHOLE_BLOOD <- "/data/storage/public/dbgap-8834/whole_blood_rna/whole_blood"
-
-# File names
-UNIQUE_FILE <- "Blood_Brain_Table.csv"
-NOT_UNIQUE_FILE <- "All_Samples_Blood_Brain_Table.csv"
+FILE_NAME <- "Blood_Brain_Table.csv"
 
 library(jsonlite)
 library(stringr)
 
 # Read in Metadata.csv
-meta <- read.csv(METADATA, header = TRUE)
+Meta <- read.csv(METADATA, header = TRUE)
 
 # Read in config containing dictionary mapping GTEx IDs to Illumina SRA IDs 
-map_lst <- fromJSON(GTEX_SRA_CONFIG)
+Map_Lst <- fromJSON(GTEX_SRA_CONFIG)
 
 # Read in list of whole blood SRA IDs
 SRA_Whole_Blood <- list.files(path = WHOLE_BLOOD, pattern = "*_1.fastq", all.files = TRUE)
@@ -25,7 +22,7 @@ SRA_Whole_Blood <- list.files(path = WHOLE_BLOOD, pattern = "*_1.fastq", all.fil
 SRA_Whole_Blood <- str_remove_all(SRA_Whole_Blood, "_1.fastq")
 
 # Make named character vector: Names are GTEx IDs, values are SRA IDs
-All_GTEx_IDs <- sapply(map_lst, '[[', 9)
+All_GTEx_IDs <- sapply(Map_Lst, '[[', 9)
 
 # Check that the whole blood samples on agave are in the GTEx sample list
 SRA_Whole_Blood %in% All_GTEx_IDs # TRUE
@@ -34,7 +31,7 @@ SRA_Whole_Blood %in% All_GTEx_IDs # TRUE
 GTEx_Whole_Blood <- names(All_GTEx_IDs[(which(SRA_Whole_Blood %in% All_GTEx_IDs))])
  
 # Make table
-Tmp_Mat <- rbind(meta[,1:2], cbind(Sample=GTEx_Whole_Blood,Tissue="Blood")) # Drop 'Age' and 'Sex' columns
+Tmp_Mat <- rbind(Meta[,1:2], cbind(Sample=GTEx_Whole_Blood,Tissue="Blood")) # Drop 'Age' and 'Sex' columns
 
 Tmp_Mat$Sample <- str_extract(Tmp_Mat$Sample,"GTEX-[0-9A-Z]+") # Filter for only individual ID
 
@@ -45,8 +42,6 @@ Sample_Tble <- as.data.frame.matrix(table(Unique_Mat))
 Compare_Tble <- as.data.frame.matrix(table(Tmp_Mat)) # Without filtering for unique IDs
 
 # Write to file
-write.csv(Sample_Tble, file = UNIQUE_FILE)
-
-write.csv(Compare_Tble, file = NOT_UNIQUE_FILE)
+write.csv(Compare_Tble, file = FILE_NAME)
 
 
